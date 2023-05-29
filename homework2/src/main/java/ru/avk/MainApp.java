87646efa556aa17a98dc4caad9f7a8ce701cde2c
AleksandrKrivoruchko <avk741120@yandex.ru>
2023-05-29 15:34:53 +0300

    An attempt to build the simplest algorithn for the game with a computer

--- homework2/src/main/java/ru/avk/MainApp.java
+++ homework2/src/main/java/ru/avk/MainApp.java
@@ -16,6 +16,8 @@ public class MainApp {
 
     // Ход игрока movePlayer[0] = x, movePlayer[1] = y
     private static int[] movePlayer = new int[2];
+
+
     public static void main(String[] args) {
         do {
             initialize();
@@ -52,20 +54,20 @@ public class MainApp {
      */
     private static void printField() {
         System.out.print("+");
-        for (int i = 0; i < fieldSizeY * 2 + 1; i++) {
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++) {
             System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
         }
         System.out.println();
 
-        for (int i = 0; i < fieldSizeX; i++) {
+        for (int i = 0; i < fieldSizeY; i++) {
             System.out.print(i + 1 + "|");
-            for (int j = 0; j < fieldSizeY; j++) {
-                System.out.print(field[i][j] + "|");
+            for (int j = 0; j < fieldSizeX; j++) {
+                System.out.print(field[j][i] + "|");
             }
             System.out.println();
         }
 
-        for (int i = 0; i < fieldSizeY * 2 + 2; i++) {
+        for (int i = 0; i < fieldSizeX * 2 + 2; i++) {
             System.out.print("-");
         }
         System.out.println();
@@ -80,6 +82,8 @@ public class MainApp {
             System.out.print("Введите координаты X и Y (от 1 до 5) через пробел >>> ");
             movePlayer[0] = SCANNER.nextInt() - 1;
             movePlayer[1] = SCANNER.nextInt() - 1;
+//            movePlayer[0] = random.nextInt(fieldSizeX);
+//            movePlayer[1] = random.nextInt(fieldSizeY);
         } while (!isCellValid() || !isCellEmpty());
         field[movePlayer[0]][movePlayer[1]] = DOT_HUMAN;
     }
@@ -98,6 +102,76 @@ public class MainApp {
     }
 
     private static void aiTurn() {
+        System.out.println("movePlayer: " + movePlayer[0] + ", " + movePlayer[1]);
+        int winCount = 2;
+        int[] tempMove = new int[2];
+        tempMove[0] = movePlayer[0];
+        tempMove[1] = movePlayer[1];
+        if(checkHorizontal(DOT_HUMAN, winCount)) {
+            System.out.println("checkHorizontal: " + movePlayer[0] + ", " + movePlayer[1]);
+            movePlayer[0] = tempMove[0] + 1;
+            if (isCellValid() && isCellEmpty()) {
+                field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                System.out.println("checkHorizontal1-2: " + movePlayer[0] + ", " + movePlayer[1]);
+                return;
+            }
+            movePlayer[0] = tempMove[0] - 1;
+            if (isCellValid() && isCellEmpty()) {
+                field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                System.out.println("checkHorizontal1-3: " + movePlayer[0] + ", " + movePlayer[1]);
+                return;
+            }
+            movePlayer[0] = tempMove[0];
+        }
+
+        if (checkVertical(DOT_HUMAN, winCount)) {
+            movePlayer[1] = tempMove[1] + 1;
+            System.out.println("checkVertical: " + movePlayer[0] + ", " + movePlayer[1]);
+            if (isCellValid() && isCellEmpty()) {
+                field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                System.out.println("checkVertical1: " + movePlayer[0] + ", " + movePlayer[1]);
+                return;
+            }
+            movePlayer[1] = tempMove[1] - 1;
+            if (isCellValid() && isCellEmpty()) {
+                field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                System.out.println("checkVertical2: " + movePlayer[0] + ", " + movePlayer[1]);
+                return;
+            }
+            movePlayer[1] = tempMove[1];
+        }
+
+        if (checkLeftDiagonal(DOT_HUMAN, winCount)) {
+            movePlayer[0] = tempMove[0] + 1;
+            movePlayer[1] = tempMove[1] + 1;
+            if (isCellValid() && isCellEmpty()) {
+                    field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                    return;
+            }
+            movePlayer[0] = tempMove[0] - 1;
+            movePlayer[1] = tempMove[1] - 1;
+            if (isCellValid() && isCellEmpty()) {
+                        field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                        return;
+            }
+            movePlayer[0] = tempMove[0];
+            movePlayer[1] = tempMove[1];
+        }
+
+        if (checkRightDiagonal(DOT_HUMAN, winCount)) {
+            movePlayer[0] = tempMove[0] - 1;
+            movePlayer[1] = tempMove[1] + 1;
+            if (isCellValid() && isCellEmpty()) {
+                    field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                    return;
+            }
+            movePlayer[0] = tempMove[0] + 1;
+            movePlayer[1] = tempMove[1] - 1;
+            if (isCellValid() && isCellEmpty()) {
+                        field[movePlayer[0]][movePlayer[1]] = DOT_AI;
+                        return;
+            }
+        }
         do {
             movePlayer[0] = random.nextInt(fieldSizeX);
             movePlayer[1] = random.nextInt(fieldSizeY);
@@ -105,16 +179,16 @@ public class MainApp {
         field[movePlayer[0]][movePlayer[1]] = DOT_AI;
     }
 
-    private static boolean checkWin(char c, int winCount) {
-        if (checkVertical(c, winCount)) return true;
-        if (checkHorizontal(c, winCount)) return true;
-        if (checkLeftDiagonal(c, winCount)) return true;
-        if (checkRightDiagonal(c, winCount)) return true;
+    private static boolean checkWin(char c) {
+        if (checkVertical(c, WIN_COUNT)) return true;
+        if (checkHorizontal(c, WIN_COUNT)) return true;
+        if (checkLeftDiagonal(c, WIN_COUNT)) return true;
+        if (checkRightDiagonal(c, WIN_COUNT)) return true;
 
         return false;
     }
 
-    private static boolean checkVertical(char c, int winCount) {
+    private static boolean checkHorizontal(char c, int winCount) {
         int count = 1;
         for (int i = movePlayer[0] + 1; i < fieldSizeX; i++) {
             if (field[i][movePlayer[1]] == c) {
@@ -139,7 +213,7 @@ public class MainApp {
         return false;
     }
 
-    private static boolean checkHorizontal(char c, int winCount) {
+    private static boolean checkVertical(char c, int winCount) {
         int count = 1;
         for (int i = movePlayer[1] + 1; i < fieldSizeY; i++) {
             if (field[movePlayer[0]][i] == c) {
@@ -230,7 +304,7 @@ public class MainApp {
     }
 
     private static boolean gameCheck(char c, String str) {
-        if (checkWin(c, WIN_COUNT)) {
+        if (checkWin(c)) {
             System.out.println(str);
             return true;
         }
