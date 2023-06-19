2862c4e96221246273a6c0bfcbfa70462d368651
AleksandrKrivoruchko <avk741120@yandex.ru>
2023-06-19 21:21:50 +0300

    Trial version of task3 homework5

--- homework5/src/main/java/ru/avk/GameBoard.java
+++ homework5/src/main/java/ru/avk/GameBoard.java
@@ -3,16 +3,24 @@ package ru.avk;
 import java.util.Random;
 
 public class GameBoard {
-    private final String[] board = new String[9];
+    private final int[] boardInt = new int[9];
+
+    private final int E = 0;
+    private final int X = 1;
+    private final int O = 2;
 
     public GameBoard() {
         fillBoard();
     }
 
-    public void printBoard() {
+    public int[] getBoardInt() {
+        return boardInt;
+    }
+
+    public void printBoard(int[] array) {
         int count = 1;
-        for (int i = 0; i < board.length; i++, count++) {
-            System.out.print(board[i] + " ");
+        for (int i = 0; i < array.length; i++, count++) {
+            System.out.print(convertIntToString(array[i]) + " ");
             if (count == 3) {
                 count = 0;
                 System.out.println();
@@ -21,13 +29,38 @@ public class GameBoard {
     }
     private void fillBoard() {
         Random random = new Random();
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 0; i < boardInt.length; i++) {
             int n = random.nextInt(3);
-            switch (n) {
-                case 0 -> board[i] = "E";
-                case 1 -> board[i] = "X";
-                case 2 -> board[i] = "O";
-            }
+            boardInt[i] = n;
         }
+    }
+
+    private String convertIntToString(int n) {
+        return switch (n) {
+            case E -> "E";
+            case X -> "X";
+            default -> "O";
+        };
+    }
+
+    public int convertBoardToInt() {
+        int result = 0;
+        int shift = 16;
+        for (int i = boardInt.length - 1; i >= 0; i--) {
+            result += boardInt[i] << shift;
+            shift -= 2;
+        }
+        return result;
+    }
+
+    public int[] convertIntToBoard(int n) {
+        int[] array = new int[9];
+        int mask = 3;
+        int shift = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (n & (mask << shift)) >> shift;
+            shift += 2;
+        }
+        return array;
     }
 }
--- homework5/src/main/java/ru/avk/MainApp.java
+++ homework5/src/main/java/ru/avk/MainApp.java
@@ -1,6 +1,7 @@
 package ru.avk;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -8,6 +9,11 @@ public class MainApp {
 //        System.out.println("*******************************");
 //        Tree.print(new File("./backup"), "", true);
         GameBoard gb = new GameBoard();
-        gb.printBoard();
+        gb.printBoard(gb.getBoardInt());
+        int n = gb.convertBoardToInt();
+        System.out.println(n);
+        gb.printBoard(gb.convertIntToBoard(n));
+        System.out.println(Arrays.toString(gb.getBoardInt()));
+        System.out.println(Arrays.toString(gb.convertIntToBoard(n)));
     }
 }
